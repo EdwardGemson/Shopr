5b46a2908ebf2ad5764eb9f4ebfb4b86cf90537c
Edward Gemson <edwardgemson@Edwards-MacBook-Pro.local>
2016-10-09 12:00:11 -0400

    remove upvotes

--- db/migrate/20160325021648_create_stores.rb
+++ db/migrate/20160325021648_create_stores.rb
@@ -5,7 +5,6 @@ class CreateStores < ActiveRecord::Migration
       t.boolean :open, null: false
       t.string :hours, null: false
       t.integer :business_id, null: false
-      t.integer :upvote_total
       t.timestamps null: false
     end
   end
