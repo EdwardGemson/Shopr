b21828434103ab9fa25d6558c35bc26fa2df53e3
edwardgemson <egemson@skidmore.edu>
2016-10-09 12:25:58 -0400

    add validatons for users, outline methods.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
+	validates :username, presence: true
+	has_secure_password
+	has_many :searches
+	has_many :businesses
+	has_many :stores, through: :businesses
+	has_many :products, through: :stores
 
+	def create_business
+		#method to be called in controller where a user can fill out a form to create businesses
+	end
 end
