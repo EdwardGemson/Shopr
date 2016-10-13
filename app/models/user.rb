58e8d7a0ed42d9950aa43026b8f90653cd2956c3
edwardgemson <egemson@skidmore.edu>
2016-10-13 11:54:58 -0400

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,12 @@ class User < ActiveRecord::Base
 	def create_business
 		#method to be called in controller where a user can fill out a form to create businesses
 	end
+
+	def create_store
+		#allows a user who owns a business (need some permissions here) to create a store with a specific location
+	end
+
+	def add_products
+		#allows a user who owns a business and store to add products to the inventory of said store
+	end 
 end
