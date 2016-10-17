f53dfa73d93f97c184e789c0faeb448b808a9273
edwardgemson <egemson@skidmore.edu>
2016-10-17 13:58:17 -0400

    Configure routes, initial css styling.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325021655) do
+ActiveRecord::Schema.define(version: 20161009153944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,20 +31,26 @@ ActiveRecord::Schema.define(version: 20160325021655) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "searches", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "category",   null: false
+    t.string   "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "stores", force: :cascade do |t|
-    t.string   "name",         null: false
-    t.boolean  "open",         null: false
-    t.string   "hours",        null: false
-    t.integer  "business_id",  null: false
-    t.integer  "upvote_total"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.string   "name",        null: false
+    t.boolean  "open",        null: false
+    t.string   "hours",       null: false
+    t.integer  "business_id", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
     t.string   "password_digest", null: false
-    t.integer  "upvote_total"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
