7a243bac265bf3cd5749a7bfb4d67fdb6027e129
edwardgemson <egemson@skidmore.edu>
2016-10-29 11:36:12 -0400

    setup elastic search, basic search bar

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,11 @@
 class SearchesController < ApplicationController
 	
 	def homepage
-		
+		if params[:query].present?
+			@stores = Store.search(params[:query], location: params[:location])
+		else
+			flash[:notice] = "Sorry, no stores have that item. Please revise your search terms"
+		end
 	end
 
 	def create
