4fc4d1bfdc3b4e26ff31b0dce919f95aa0e8c12d
edwardgemson <egemson@skidmore.edu>
2016-10-29 12:56:56 -0400

    update seed file

--- db/migrate/20161029161009_create_inventories.rb
+++ db/migrate/20161029161009_create_inventories.rb
@@ -2,7 +2,7 @@ class CreateInventories < ActiveRecord::Migration
   def change
     create_table :inventories do |t|
     	t.integer :store_id, null: false
-    	t.string :products, null: false
+    	t.text :products, null: false
     	t.timestamps null: false
     end
   end
