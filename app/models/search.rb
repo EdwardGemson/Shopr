90f70fc9d87f1801167cda2cd825cf2729f59beb
edwardgemson <egemson@skidmore.edu>
2017-02-15 13:30:14 -0500

    complete item searching with walmart API

--- app/models/search.rb
+++ app/models/search.rb
@@ -8,12 +8,15 @@ class Search < ActiveRecord::Base
 
 	def self.search_walmart(item, current_user)
 		search_results = []
-		new_search = Search.create(name: item, user_id: current_user.id)
+		new_search = Search.find_or_create_by(name: item, user_id: current_user.id)
 		response = HTTParty.get('http://api.walmartlabs.com/v1/search?query=item&format=json&apiKey=z42q96ksvaqwy6v86pzz5phx')
-		response['items'].each do |product|
-			search_results << Product.find_or_initialize_by(name: product['name'], store_name: "walmart").update_attributes(price: product['salePrice'].to_i)
+		search_results << response['items'].select do |product|
+			product['name'] = new_search.name
 		end
 		search_results
 	end
 
 end
+
+
+#search_results << Product.find_or_create_by(name: new_search['name'], store_name: "walmart").update_attributes(price: new_search['salePrice'].to_i)
